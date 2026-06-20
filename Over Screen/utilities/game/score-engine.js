e7abeccaaa99efd1814563e4b5df7b47801dbd0e
sbarathsubramani2005-byte <sbarathsubramani2005@gmail.com>
2026-06-20 13:46:36 +0530

    added prompt matching

--- Over Screen/utilities/game/score-engine.js
+++ Over Screen/utilities/game/score-engine.js
@@ -1,11 +1,1 @@
- const score =Math.round((matched.length /originalWords.length) * 100);
-
-    document.getElementById("score").innerText = score;
-
-    document.getElementById("currentScore").innerText = score + "%";
-
-    document.getElementById("matchedWords").innerText = matched.length;
-
-    document.getElementById("matchedList").innerText =matched.length > 0 ? matched.join(", ") : "No Match";
-
 
